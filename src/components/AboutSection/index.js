7a880c46538ebf16afd71e2ae6811a480d239076
Moe-Reda <108328374+Moe-Reda@users.noreply.github.com>
2023-05-08 16:20:22 -0400

    Fixed about section, title and icon

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -6,8 +6,8 @@ const About = () =>{
     <AboutContainer id='about'>
         <Headline>About me:</Headline>
         <Text>
-            I am a student at the University of Toronto currently looking for an intership for spring 2023. 
-            I enjoy creating apps and solving complex problems, in addition to this I am really eager to learn new technologies. 
+            I am a student at the University of Toronto currently looking for an intership for spring 2024. 
+            I enjoy creating apps, solving complex problems and working with bright minds, in addition to this I am really eager to learn new technologies. 
         </Text>
     </AboutContainer>
     )
