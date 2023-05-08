a2ecb2121a217efa3ff91b1f19235aedeecefa5e
Moe-Reda <108328374+Moe-Reda@users.noreply.github.com>
2023-05-08 15:52:43 -0400

    Update after 2022-2023 school year.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -16,6 +16,24 @@ const Projects = () =>{
                         <p>React</p>
                     </TechnologiesContainer>
             </Projectcard>
+            <Projectcard>
+                <ProjectName>Chat Server</ProjectName>
+                    <ProjectDescription>
+                        For the CSC209 course (Software tools and systems). I programed a chat server in C that uses a TCP connection and does not block.
+                    </ProjectDescription>
+                    <TechnologiesContainer >
+                        <p>C</p>
+                    </TechnologiesContainer>
+            </Projectcard>
+            <Projectcard>
+                <ProjectName>Simon's Game</ProjectName>
+                    <ProjectDescription>
+                        For the CSC258 course (Computer Organization). I made the notoriously known "Simon's Game" using an Assembly language (RISC-V). In addition to this I also wrote a technical documentation for it.
+                    </ProjectDescription>
+                    <TechnologiesContainer >
+                        <p>Assembly</p>
+                    </TechnologiesContainer>
+            </Projectcard>
         </CardContainer>
     </ProjectsContainer>
     )
